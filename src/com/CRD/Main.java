4cc60582a773f8e62f2abbe2f03e7cac1dd04255
Sagar Mangulkar <smangul1@binghamton.edu>
2018-09-08 00:04:25 -0400

    Implement Car Rental System with console

--- src/com/CRD/Main.java
+++ src/com/CRD/Main.java
@@ -2,6 +2,8 @@ package com.CRD;
 
 import com.CRD.Beans.CarType;
 
+import javax.swing.*;
+import java.awt.*;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -13,18 +15,59 @@ public class Main {
         Receptionist receptionist = new Receptionist();
         receptionist.countCarsInitially();
 
-        System.out.println("Welcome to Car Rental System...!\n Kindly provide date and time in below format \nMM-dd-yyyy hh:mm:ss PM\nOr\nMM-dd-yyyy hh:mm:ss AM");
-        Scanner scanner = new Scanner(System.in);
-        Date pickUpDate = getPickUpDateFromUser(scanner);
-        System.out.println(pickUpDate);
-        System.out.println("Enter number of days you require Car: ");
-        int numberOfDays = Integer.parseInt(scanner.next());
-        System.out.println(numberOfDays);
 
-        String carTypeString = scanner.next();
-        CarType carType = CarType.valueOf(carTypeString.toUpperCase());
-        String message = receptionist.createReservation(pickUpDate, numberOfDays, carType);
-        System.out.println(message);
+        JLabel label = new JLabel("java2s.com");
+
+        JFrame frame = new JFrame("Testing");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+        frame.add(label);
+
+        JPanel buttons = new JPanel(new GridLayout(0, 1));
+        for (int index = 0; index < 10; index++) {
+            buttons.add(new JButton(String.valueOf(index)));
+        }
+
+        JPanel right = new JPanel(new BorderLayout());
+        right.add(buttons, BorderLayout.NORTH);
+        frame.add(right, BorderLayout.EAST);
+
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+
+        while (true) {
+            System.out.println("\n******** Welcome to Car Rental System ***********\n\n" +
+                    "Kindly provide date and time in below format \n" +
+                    "MM-dd-yyyy hh:mm:ss PM\n" +
+                    "Or\n" +
+                    "MM-dd-yyyy hh:mm:ss AM");
+            Scanner scanner = new Scanner(System.in);
+            Date pickUpDate = getPickUpDateFromUser(scanner);
+            System.out.println("Enter number of days you require Car: ");
+            int numberOfDays = Integer.parseInt(scanner.next());
+
+            System.out.println("Enter Car type from options:");
+            int i = 0;
+            for (CarType car : Receptionist.getAvailableCars().keySet()) {
+                System.out.println(++i + ". " + car);
+            }
+            String carTypeString = scanner.next();
+            CarType carType = CarType.valueOf(carTypeString.toUpperCase());
+            boolean isAvailable = receptionist.checkReservationAvailability(pickUpDate, numberOfDays, carType);
+            if (isAvailable) {
+                boolean isCreated = receptionist.createReservation(pickUpDate, numberOfDays, carType);
+                if (isCreated) {
+                    System.out.println("Reservation successfully created, Thank you.");
+                }
+                else {
+                    System.out.println("Error in creating reservation, kindly try again.");
+                }
+            }
+            else {
+                System.out.println("Sorry, " + carType + " is not available in given days. Kindly choose other Car type or other days.");
+            }
+        }
     }
 
     private static Date getPickUpDateFromUser(Scanner scanner) {
--- src/com/CRD/Receptionist.java
+++ src/com/CRD/Receptionist.java
@@ -3,24 +3,93 @@ package com.CRD;
 import com.CRD.Beans.CarType;
 import com.CRD.Beans.Reservation;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Hashtable;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
 public class Receptionist {
-    public String createReservation(Date pickUpDate, int numberOfDays, CarType carType) {
-        Reservation reservation = new Reservation(pickUpDate, numberOfDays, carType);
-        return "temp";
+    public boolean checkReservationAvailability(Date pickUpDate, int numberOfDays, CarType carType) {
+        DateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+        Date pickUpDateWithZeroTime = null;
+        try {
+            pickUpDateWithZeroTime = formatter.parse(formatter.format(pickUpDate));
+            for (int i = 0; i < numberOfDays; i++) {
+                String key = pickUpDateWithZeroTime.toString();
+                if(reservations.containsKey(key)) {
+                    List<Reservation> reservationList = reservations.get(key);
+                    int carCount = availableCars.get(carType);
+                    for (Reservation reservation : reservationList) {
+                        if (reservation.getCarType().equals(carType)) {
+                            carCount--;
+                        }
+                        if (carCount == 0) {
+                            return false;
+                        }
+                    }
+                }
+                pickUpDateWithZeroTime = getIncrementedDate(pickUpDateWithZeroTime);
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return true;
+    }
+
+    public boolean createReservation(Date pickUpDate, int numberOfDays, CarType carType) {
+        Reservation reservation = new Reservation(pickUpDate,numberOfDays, carType);
+        DateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+        Date pickUpDateWithZeroTime = null;
+        try {
+            pickUpDateWithZeroTime = formatter.parse(formatter.format(pickUpDate));
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return false;
+        }
+
+        for (int i = 0; i < numberOfDays; i++) {
+            String key = pickUpDateWithZeroTime.toString();
+            if (reservations.containsKey(key)) {
+                List<Reservation> reservationList = reservations.get(key);
+                reservationList.add(reservation);
+                reservations.put(key, reservationList);
+            } else {
+                List<Reservation> reservationList = new LinkedList<>();
+                reservationList.add(reservation);
+                reservations.put(key, reservationList);
+            }
+            pickUpDateWithZeroTime = getIncrementedDate(pickUpDateWithZeroTime);
+        }
+        return true;
+    }
+
+    public static Date getIncrementedDate(Date date)
+    {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        cal.add(Calendar.DATE, ONE);
+        return cal.getTime();
     }
 
     public void countCarsInitially() {
-        AVAILABLE_CARS = new Hashtable<>();
-        AVAILABLE_CARS.put(CarType.SEDAN, 6);
-        AVAILABLE_CARS.put(CarType.SUV, 3);
-        AVAILABLE_CARS.put(CarType.VAN, 2);
+        availableCars = new Hashtable<>();
+        availableCars.put(CarType.SEDAN, 6);
+        availableCars.put(CarType.SUV, 3);
+        availableCars.put(CarType.VAN, 2);
+
+        reservations = new Hashtable<>();
     }
 
-    public static Map<CarType, Integer> AVAILABLE_CARS;
-    public static Map<String, List<Reservation>> RESEREVATIONS;
+    public static Map<CarType, Integer> getAvailableCars() {
+        return availableCars;
+    }
+
+    private static Map<CarType, Integer> availableCars;
+    private static Map<String, List<Reservation>> reservations;
+    private static final int ONE = 1;
 }
